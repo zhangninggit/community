686e59a57811a8d3273ef555beea7e5850202825
xiaoning <zhangning2017@163.com>
2019-05-10 14:23:50 +0800

    配置登录接口及application.properties文件

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 ## 资料
 
+https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/
+
 ## 工具
 
---
+++ src/main/java/life/majiang/community/Controller/AuthorizeController.java
@@ -0,0 +1,44 @@
+package life.majiang.community.Controller;
+
+
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientID;
+
+    @Value("${github.Client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state" )String state){
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id(clientID);
+        accessTokenDTO.setClient_secret(clientSecret);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+        }
+}
