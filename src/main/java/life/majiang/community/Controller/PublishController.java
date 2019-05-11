f5b1372dad5d8a1aa75833e639312b95f6ca939a
xiaoning <zhangning2017@163.com>
2019-05-12 00:45:26 +0800

    add publish page

---
+++ src/main/java/life/majiang/community/Controller/PublishController.java
@@ -0,0 +1,17 @@
+package life.majiang.community.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublishController {
+
+    @GetMapping("/publish")
+    public String publishController()
+    {
+
+
+        return "publish";
+    }
+}
